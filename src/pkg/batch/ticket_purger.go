efef9784d7129492e29cf830fde1b51f5452dceb
Alberto De Saint Malo <beto7299@gmail.com>
2025-10-03 17:57:14 -0400

    only unchecking not checked

--- src/pkg/batch/ticket_purger.go
+++ src/pkg/batch/ticket_purger.go
@@ -26,8 +26,10 @@ func PurgeTickets(ctx context.Context, cfg pkg.Config) {
 		panic(err)
 	}
 	for _, v := range tick {
-		logger.Logger.Debug("Unchecking ticket", zap.Any("ticketId", v.ID))
-		tickets.CheckInTicket(ctx, ticketTailorConfig, v.ID, tickets.CheckOut)
+		if v.CheckedIn == "false" {
+			logger.Logger.Debug("Unchecking ticket", zap.Any("ticketId", v.ID))
+			tickets.CheckInTicket(ctx, ticketTailorConfig, v.ID, tickets.CheckOut)
+		}
 	}
 
 }
